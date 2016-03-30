ce1d5f73701bb7097c382718deba3102b90337e2
geekdeath <antoine@martin-julien.fr>
2016-03-30 16:13:27 +0200

    Retrait data
    Retrait data

--- SERVER/backups/bob/toto/current/var/www/CC/scripts/scp/CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-# Changelog
-
-## 0.9.0 (2015-02-04)
-
-- Add changelog
-- Finish up py3k and unicode support
-- Unicode should work on OSX, Windows and Linux
-- Some tests have been added
-
--- SERVER/backups/bob/var/www/CC/website/tmp/var/www/CC/website/include/footer.php
+++
@@ -1,9 +0,0 @@
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script>
-  $("#deconnexion").click(function(){
-    alert("Deconnexion");
-  })
-  </script>
- </body>
-</html>
